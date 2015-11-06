321a8fb9f5d5a715010fe1a692d44a112db5b24a
Ahmed Abdel-wahab <ahmed.abdel-wahab@robustastudio.com>
2015-11-06 20:48:11 +0200

    added test files

--- src/test/AlgorithmsTest.java
+++ src/test/AlgorithmsTest.java
@@ -1,69 +1,12 @@
-/**
- * 
- */
 package test;
 
-import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-
-import main.Board;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-/**
- * @author MacBookAir
- *
- */
-
+@RunWith(Suite.class)
+@SuiteClasses({ AStarStrategy.class, BFSStrategy.class, DFSStrategy.class,
+		GreedyH1Strategy.class, GreedyH2Strategy.class })
 public class AlgorithmsTest {
-	public String fileName = "boards";
-
-	public void readFile() {
-		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-			String line = br.readLine();
-			int numberOfBoards = Integer.parseInt(line);
-			for (int i = 1; i < numberOfBoards; i++) {
-				line = br.readLine();
-				String[] dimensions = line.split(" ");
-				int rows = Integer.parseInt(dimensions[0]);
-				int cols = Integer.parseInt(dimensions[1]);
-				Board b = new Board(rows, cols);
-				b.grid[0][1] = 0;
-			}
-			// line is not visible here.
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
 
 }
